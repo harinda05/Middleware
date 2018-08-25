98cbc1b6a6120f520b66358251222fd392ef05ac
Kavinda Jayakody <kavindajayakody@gmail.com>
2018-08-26 01:15:21 +0530

    Service registry class skelton added (Remove service part)

--- ServiceRegistry/src/com/registry/service/ServiceRegistry.java
+++ ServiceRegistry/src/com/registry/service/ServiceRegistry.java
@@ -1,5 +1,7 @@
 package com.registry.service;
 
+import org.omg.CORBA.PUBLIC_MEMBER;
+
 import java.util.HashMap;
 import java.util.Map;
 
@@ -10,7 +12,15 @@ public class ServiceRegistry {
     public static void main(String[] args) {
         ServiceRegistry sr = new ServiceRegistry();
         sr.addService();
-        System.out.println(sr.getService("usd"));
+        System.out.println("The location of usd service : " + sr.getService("usd"));
+        System.out.println("----------------------------------------------");
+        System.out.println("Removing service xyz is " + sr.removeService("xyz"));
+        System.out.println("----------------------------------------------");
+        System.out.println("Calling xyz service again");
+        System.out.println("The location of xyz : " + sr.getService("xyz"));
+        System.out.println("----------------------------------------------");
+        System.out.println("Removing service xyz again is " + sr.removeService("xyz"));
+
     }
 
     public void addService() {
@@ -24,6 +34,22 @@ public class ServiceRegistry {
     public String getService(String service) {
 
         return registry.get(service);
+
+    }
+
+    public boolean removeService(String service) {
+
+        if(registry.containsKey(service)) {
+            try {
+                registry.remove(service);
+                return true;
+            } catch (NullPointerException  e) {
+                return false;
+            }
+        } else {
+            System.out.println("No services matching " + service);
+            return false;
+        }
 
     }
 
