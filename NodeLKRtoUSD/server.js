5a61901a90f353045b8947488f5defb1630aa8b0
harinda05 <harindaburndown@gmail.com>
2018-08-25 22:29:24 +0530

    initial commit

---
+++ NodeLKRtoUSD/server.js
@@ -0,0 +1,13 @@
+var currencyConverter = require ('./routes/currencyConverter');
+const bodyParser = require('body-parser');
+
+var express = require('express'),
+  app = express();
+const port = process.env.PORT || 5000
+app.listen(port);
+app.use(bodyParser.json());
+
+app.use('/', currencyConverter);
+console.log('RESTful API server started on: ' + port);
+
+
