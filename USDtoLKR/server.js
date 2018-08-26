5cf929990dd9b2a9a0eff777de2ce530d51768dd
harinda05 <harindaburndown@gmail.com>
2018-08-26 19:32:08 +0530

    Testing main method

--- USDtoLKR/server.js
+++ USDtoLKR/server.js
@@ -10,4 +10,27 @@ app.use(bodyParser.json());
 app.use('/', currencyConverter);
 console.log('RESTful API server started on port : ' + port);
 
-
+// var data = [{
+//   "Service_Name" : "UsdtoLkrService",
+//   "Service_Call_Name" :  "usdtolkr",
+//   "Configs" : {
+//     "location"  : "https://usdtolkrservice.herokuapp.com/",
+//     "port" : port,
+//     "type" : "REST",
+//     "method" : "POST",
+//     "parameters" : {
+//       "parameter_01" : "number",
+//     }
+//   }
+// }
+//   ];
+ 
+// fetch('http://localhost:3000/test', {
+//     method: 'POST',
+//     body: JSON.stringify(data),
+//     headers: {'Content-Type': 'application/json'}
+// }).then(data => { 
+//   alert(JSON.stringify(data));
+// }).catch(err => {
+//     alert(err);
+// });
